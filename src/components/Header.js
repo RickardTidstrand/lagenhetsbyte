88aae7b85577091f0237f9c595e7f85089a80c12
Rickard <rickard.tidstrand@axakon.se>
2019-05-19 18:01:40 +0200

    set norris joke snackbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import NorrisSnackbar from './NorrisSnackbar'
 
 import '../css/header.css'
 import HeaderImg from '../resources/img/axakon.png'
 
-const Header =()=>{
-
+function Header(){
   return(
     <header>
+      <NorrisSnackbar/>
       <img src={HeaderImg} alt="header"/>
     </header>
   )
